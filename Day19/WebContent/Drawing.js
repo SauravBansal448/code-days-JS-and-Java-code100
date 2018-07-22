4a910ebba807acfcdb8881904949b782abbbc449
Saurav Bansal <40735538+SauravBansal448@users.noreply.github.com>
2018-07-22 22:39:47 +0530

    Day 19

---
+++ Day19/WebContent/Drawing.js
@@ -0,0 +1,10 @@
+function draw() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var context = canvas.getContext('2d');
+
+    context.fillRect(20,20,100,100);
+    context.clearRect(40,40,60,60);
+    context.strokeRect(45,45,50,50);
+  }
+}
