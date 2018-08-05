e444b985a59c35167d59d28b10f92f5b1dd4bc2f
Saurav Bansal <40735538+SauravBansal448@users.noreply.github.com>
2018-08-05 23:38:32 +0530

    Day 34

---
+++ Day 34/hackthrow.js
@@ -0,0 +1,14 @@
+function isPositive(a) {
+    if (a > 0)
+        {
+            return "YES";
+        }
+    else if (a === 0)
+        {
+            throw new Error("Zero Error");
+        }
+    else if (a < 0)
+        {
+            throw new Error("Negative Error");
+        }
+}
