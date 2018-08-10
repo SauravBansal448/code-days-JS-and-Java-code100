5cf0559203f7c17bc99dcf3c2ef7da497c591521
Saurav Bansal <40735538+SauravBansal448@users.noreply.github.com>
2018-08-10 19:29:30 +0530

    Day 39

---
+++ Day39/trycatch.js
@@ -0,0 +1,15 @@
+function reverseString(s) {
+     try {
+        var splitString = s.split("");
+        var reverseArray = splitString.reverse();
+        var joinArray = reverseArray.join("");
+        console.log(joinArray);
+    }
+    catch (error)
+        {
+            // Print s on a new line. If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
+            console.log(error.message);
+            console.log(s);
+        }
+    
+}
