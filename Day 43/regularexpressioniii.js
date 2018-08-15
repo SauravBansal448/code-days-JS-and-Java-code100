4d0c8a16c6540525cb6d82a78fa88313d9d2b70c
Saurav Bansal <40735538+SauravBansal448@users.noreply.github.com>
2018-08-15 22:27:52 +0530

    Day 43

---
+++ Day 43/regularexpressioniii.js
@@ -0,0 +1,16 @@
+//  Regular Expressions III
+
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    
+    
+    var re = RegExp('\\d+', 'g')
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
