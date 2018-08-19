c25de2fc27c32445bc78068260c63932363f1feb
Saurav Bansal <40735538+SauravBansal448@users.noreply.github.com>
2018-08-19 21:12:11 +0530

    Day 47

---
+++ Day 47/Dates.js
@@ -0,0 +1,11 @@
+function getDayName(dateString) {
+    let dayName;
+    // Write your code here
+    
+    var day_names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    var date = new Date(dateString);
+    
+    dayName = day_names[date.getDay()];
+    
+    return dayName;
+}
