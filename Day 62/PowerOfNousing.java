249aaa66801655688d78407ee37f46e9893df4ee
Saurav Bansal <40735538+SauravBansal448@users.noreply.github.com>
2018-09-03 10:52:09 -0700

    Day 62

---
+++ Day 62/PowerOfNousing.java
@@ -0,0 +1,10 @@
+public class Power {
+
+    public static void main(String[] args) {
+
+        int base = 3, exponent = -4;
+        double result = Math.pow(base, exponent);
+
+        System.out.println("Answer = " + result);
+    }
+}
