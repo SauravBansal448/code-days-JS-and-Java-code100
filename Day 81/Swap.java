9b8a388942c34de4f836628b8ba11b9732a3c43a
Saurav Bansal <40735538+SauravBansal448@users.noreply.github.com>
2018-09-24 07:33:19 -0700

    Day 81

---
+++ Day 81/Swap.java
@@ -0,0 +1,23 @@
+public class SwapElementsWithoutThirdVariableExample {
+ 
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = 20;
+		
+		System.out.println("Before Swapping");
+		System.out.println("Value of num1 is :" + num1);
+		System.out.println("Value of num2 is :" +num2);
+		
+		//add both the numbers and assign it to first
+		num1 = num1 + num2;
+		num2 = num1 - num2;
+		num1 = num1 - num2;
+		
+		System.out.println("After Swapping");
+		System.out.println("Value of num1 is :" + num1);
+		System.out.println("Value of num2 is :" +num2);
+	}
+ 
+ 
+}
