8b39d3f77af415219b83c78a32b50291df4ff0b0
Saurav Bansal <40735538+SauravBansal448@users.noreply.github.com>
2018-09-28 09:00:42 -0700

    Day 85

---
+++ Day 85/OccuranceOfcharacter.java
@@ -0,0 +1,17 @@
+
+public class RemoveCharFromString {
+
+	public static void main(String[] args) {
+
+		removeCharFromString("abcbcdjfkd", 'c');
+		removeCharFromString("Pankaj", 'a');
+		removeCharFromString("ç∂©∂ç", '©');
+
+	}
+
+	private static void removeCharFromString(String input, char c) {
+		String result = input.replaceAll(String.valueOf(c), "");
+		System.out.println(result);
+	}
+
+}
