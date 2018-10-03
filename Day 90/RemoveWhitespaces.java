61299ded873ad13b403efc3aa8cc2b47fabec47f
Saurav Bansal <40735538+SauravBansal448@users.noreply.github.com>
2018-10-03 09:59:24 -0700

    Day 90

---
+++ Day 90/RemoveWhitespaces.java
@@ -0,0 +1,12 @@
+//Remove Whitespace
+
+public class Whitespaces {
+
+    public static void main(String[] args) {
+        String sentence = "T    his is b  ett     er.";
+        System.out.println("Original sentence: " + sentence);
+
+        sentence = sentence.replaceAll("\\s", "");
+        System.out.println("After replacement: " + sentence);
+    }
+}
