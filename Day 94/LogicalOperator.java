b7317a640b45d6cbfce4fbaf4c8744762da1d2a7
Saurav Bansal <40735538+SauravBansal448@users.noreply.github.com>
2018-10-07 09:37:22 -0700

    Day 94

---
+++ Day 94/LogicalOperator.java
@@ -0,0 +1,20 @@
+
+public class LogicalOperator {
+/*
+ AND &&
+ OR || 
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number1=15;
+		int number2=20;
+		if((number1>=10 || number2<=21)) {
+			System.out.println("yes! condition is true");
+			}
+		else {
+			System.out.println("No! condition is not true");
+		}
+
+	}
+
+}
