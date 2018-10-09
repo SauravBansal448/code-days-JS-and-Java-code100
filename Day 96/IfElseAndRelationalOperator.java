18094ff40ee7234c2dc7823926e5fde8d2a70f7f
Saurav Bansal <40735538+SauravBansal448@users.noreply.github.com>
2018-10-09 09:06:39 -0700

    Day 96

---
+++ Day 96/IfElseAndRelationalOperator.java
@@ -0,0 +1,24 @@
+
+public class IfElseAndRelationalOperator {
+	/*
+	 ==  is equal to
+	 !=   not equal to
+	 <    less than
+	 >    greater than
+	 <= less than equal to
+	 >= greater then equal to 
+	 */
+
+	public static void main(String[] args) {
+		int number=10;
+		// TODO Auto-generated method stub
+		if(number==10) {
+			System.out.println("yes Number is equal to 10");
+		}
+		else {
+			System.out.println("No Number is not equal to 10;");
+		}
+
+	}
+
+}
