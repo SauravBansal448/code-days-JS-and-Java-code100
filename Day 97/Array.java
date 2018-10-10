11d017015a31412fee1b0d6db77aa0b7f50a6f33
Saurav Bansal <40735538+SauravBansal448@users.noreply.github.com>
2018-10-10 08:41:28 -0700

    Day 97

---
+++ Day 97/Array.java
@@ -0,0 +1,24 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {};
+		/*
+		 * types of declaration of array in java
+		 * int arr[]={1,2,3,4,5};
+		 * int [] arr=new int[]{1,2,3,4,5};
+		 * int [] arr=new int[3];
+		 
+		 */
+		int index=0;
+
+		do {
+		System.out.println(arr[index]);
+		index++;
+		}while(index<5);
+		
+		
+	}
+
+}
