06e4a1652678f37b638f3f710c16a8e3ca430bc4
Saurav Bansal <40735538+SauravBansal448@users.noreply.github.com>
2018-10-12 07:37:28 -0700

    Day 99

---
+++ Day 99/IncremenrAndAssignmentOperator.java
@@ -0,0 +1,19 @@
+
+public class IncremenrAndAssignmentOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		
+		//x+=5;//x=x+5;
+		//x*=5;//x=x*5;
+		//x/=5;//x=x/5;
+		//x-=5;//x=x-5;
+		
+		//System.out.println(x++);
+		System.out.println(x*=5);
+
+
+	}
+
+}
